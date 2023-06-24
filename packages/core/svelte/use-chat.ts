09f6e641700da943f0ed4567369744bcef2de5f4
Jesse Pascoe <excalibr8@yahoo.com>
2023-06-23 22:34:02 -0400

    Refactor useChat to handle function calls in streamed responses
    This commit refactors the `useChat` function to handle function calls in streamed responses. It adds a new parameter, `onFunctionCall`, which is called when a function call is received from the API. The user can define their own handler for this event and return a new chat request object that will be sent back to the API. If no response is returned, the loop will break and append any remaining messages to the chat history. This change also updates some types and renames `RequestOptions` to `ChatRequestOptions`.

--- packages/core/svelte/use-chat.ts
+++ packages/core/svelte/use-chat.ts
@@ -7,9 +7,11 @@ import type {
   Message,
   CreateMessage,
   UseChatOptions,
-  RequestOptions
+  ChatRequestOptions,
+  ChatRequest
 } from '../shared/types'
 import { nanoid, createChunkDecoder } from '../shared/utils'
+import { ChatCompletionRequestMessageFunctionCall } from 'openai-edge'
 
 export type { Message, CreateMessage, UseChatOptions }
 
@@ -24,14 +26,14 @@ export type UseChatHelpers = {
    */
   append: (
     message: Message | CreateMessage,
-    options?: RequestOptions
+    options?: ChatRequestOptions
   ) => Promise<string | null | undefined>
   /**
    * Reload the last AI chat response for the given chat history. If the last
    * message isn't from the assistant, it will request the API to generate a
    * new response.
    */
-  reload: (options?: RequestOptions) => Promise<string | null | undefined>
+  reload: (options?: ChatRequestOptions) => Promise<string | null | undefined>
   /**
    * Abort the current request immediately, keep the generated tokens if any.
    */
@@ -45,9 +47,134 @@ export type UseChatHelpers = {
   /** The current value of the input */
   input: Writable<string>
   /** Form submission handler to automattically reset input and append a user message  */
-  handleSubmit: (e: any) => void
+  handleSubmit: (e: any, options?:ChatRequestOptions) => void
+  metadata?: Object
   /** Whether the API request is in progress */
   isLoading: Writable<boolean>
+}
+const getStreamedResponse = async (
+  api: string,
+  chatRequest: ChatRequest,
+  mutate: (messages: Message[]) => void,
+  headers: Record<string, string> | Headers | undefined,
+  body: any,
+  previousMessages: Message[],
+  abortControllerRef: AbortController | null,
+  onFinish?: (message: Message) => void,
+  onResponse?: (response: Response) => void | Promise<void>,
+  sendExtraMessageFields?: boolean
+) => {
+  // Do an optimistic update to the chat state to show the updated messages
+  // immediately.
+  mutate(chatRequest.messages)
+
+  const res = await fetch(api, {
+    method: 'POST',
+    body: JSON.stringify({
+      messages: sendExtraMessageFields
+        ? chatRequest.messages
+        : chatRequest.messages.map(
+            ({ role, content, name, function_call }) => ({
+              role,
+              content,
+              ...(name !== undefined && { name }),
+              ...(function_call !== undefined && {
+                function_call: function_call
+              })
+            })
+          ),
+      ...body,
+      ...chatRequest.options?.body,
+      ...(chatRequest.functions !== undefined && {
+        functions: chatRequest.functions
+      }),
+      ...(chatRequest.function_call !== undefined && {
+        function_call: chatRequest.function_call
+      })
+    }),
+    headers: {
+      ...headers,
+      ...chatRequest.options?.headers
+    },
+    ...(abortControllerRef !== null && {
+      signal: abortControllerRef.signal
+    })
+  }).catch(err => {
+    // Restore the previous messages if the request fails.
+    mutate(previousMessages)
+    throw err
+  })
+
+  if (onResponse) {
+    try {
+      await onResponse(res)
+    } catch (err) {
+      throw err
+    }
+  }
+
+  if (!res.ok) {
+    // Restore the previous messages if the request fails.
+    mutate(previousMessages)
+    throw new Error((await res.text()) || 'Failed to fetch the chat response.')
+  }
+
+  if (!res.body) {
+    throw new Error('The response body is empty.')
+  }
+
+  let streamedResponse = ''
+  const createdAt = new Date()
+  const replyId = nanoid()
+  const reader = res.body.getReader()
+  const decode = createChunkDecoder()
+
+  let responseMessage: Message = {
+    id: replyId,
+    createdAt,
+    content: '',
+    role: 'assistant'
+  }
+
+  while (true) {
+    const { done, value } = await reader.read()
+    if (done) {
+      break
+    }
+    // Update the chat state with the new message tokens.
+    streamedResponse += decode(value)
+
+    if (streamedResponse.startsWith('{"function_call":')) {
+      // While the function call is streaming, it will be a string.
+      responseMessage['function_call'] = streamedResponse
+    } else {
+      responseMessage['content'] = streamedResponse
+    }
+
+    mutate([...chatRequest.messages, { ...responseMessage }])
+
+    // The request has been aborted, stop reading the stream.
+    if (abortControllerRef === null) {
+      reader.cancel()
+      break
+    }
+  }
+
+  if (streamedResponse.startsWith('{"function_call":')) {
+    // Once the stream is complete, the function call is parsed into an object.
+    const parsedFunctionCall: ChatCompletionRequestMessageFunctionCall =
+      JSON.parse(streamedResponse).function_call
+
+    responseMessage['function_call'] = parsedFunctionCall
+
+    mutate([...chatRequest.messages, { ...responseMessage }])
+  }
+
+  if (onFinish) {
+    onFinish(responseMessage)
+  }
+
+  return responseMessage
 }
 
 let uniqueId = 0
@@ -60,6 +187,7 @@ export function useChat({
   initialMessages = [],
   initialInput = '',
   sendExtraMessageFields,
+  onFunctionCall,
   onResponse,
   onFinish,
   onError,
@@ -89,102 +217,57 @@ export function useChat({
   const isLoading = writable(false)
 
   let abortController: AbortController | null = null
-  async function triggerRequest(
-    messagesSnapshot: Message[],
-    options?: RequestOptions
-  ) {
+  async function triggerRequest(chatRequest: ChatRequest) {
     try {
       isLoading.set(true)
       abortController = new AbortController()
 
-      // Do an optimistic update to the chat state to show the updated messages
-      // immediately.
-      const previousMessages = get(messages)
-      mutate(messagesSnapshot)
-
-      const res = await fetch(api, {
-        method: 'POST',
-        body: JSON.stringify({
-          messages: sendExtraMessageFields
-            ? messagesSnapshot
-            : messagesSnapshot.map(({ role, content }) => ({
-                role,
-                content
-              })),
-          ...body,
-          ...options?.body
-        }),
-        headers: {
-          ...headers,
-          ...options?.headers
-        },
-        signal: abortController.signal
-      }).catch(err => {
-        // Restore the previous messages if the request fails.
-        mutate(previousMessages)
-        throw err
-      })
-
-      if (onResponse) {
-        try {
-          await onResponse(res)
-        } catch (err) {
-          throw err
-        }
-      }
-
-      if (!res.ok) {
-        // Restore the previous messages if the request fails.
-        mutate(previousMessages)
-        throw new Error(
-          (await res.text()) || 'Failed to fetch the chat response.'
-        )
-      }
-      if (!res.body) {
-        throw new Error('The response body is empty.')
-      }
-
-      let result = ''
-      const createdAt = new Date()
-      const replyId = nanoid()
-      const reader = res.body.getReader()
-      const decoder = createChunkDecoder()
-
       while (true) {
-        const { done, value } = await reader.read()
-        if (done) {
+        const streamedResponseMessage = await getStreamedResponse(
+          api,
+          chatRequest,
+          mutate,
+          headers, 
+          body,
+          get(messages),
+          abortController,
+          onFinish,
+          onResponse,
+          sendExtraMessageFields
+        )
+        
+        if (
+          streamedResponseMessage.function_call === undefined ||
+          typeof streamedResponseMessage.function_call === 'string'
+        ) {
           break
         }
-        // Update the chat state with the new message tokens.
-        result += decoder(value)
-        mutate([
-          ...messagesSnapshot,
-          {
-            id: replyId,
-            createdAt,
-            content: result,
-            role: 'assistant'
-          }
-        ])
 
-        // The request has been aborted, stop reading the stream.
-        if (abortController === null) {
-          reader.cancel()
-          break
+        // Streamed response is a function call, invoke the function call handler if it exists.
+        if (onFunctionCall) {
+          const functionCall = streamedResponseMessage.function_call
+
+          // User handles the function call in their own functionCallHandler.
+          // The "arguments" of the function call object will still be a string which will have to be parsed in the function handler.
+          // If the JSON is malformed due to model error the user will have to handle that themselves.
+
+          const onFunctionCallResponse = await onFunctionCall(
+            get(messages),
+            functionCall
+          )
+
+          // If the user does not return anything, the loop will break.
+          if (onFunctionCallResponse === undefined) break
+
+          // Add function call response to the chat and automatically
+          // send to the API in the next iteration of the loop.
+          chatRequest = onFunctionCallResponse
         }
-      }
-
-      if (onFinish) {
-        onFinish({
-          id: replyId,
-          createdAt,
-          content: result,
-          role: 'assistant'
-        })
       }
 
       abortController = null
-      return result
+
+      return get(messages).at(-1)?.content?? ''
     } catch (err) {
       // Ignore abort errors as they are expected.
       if ((err as any).name === 'AbortError') {
@@ -202,22 +285,38 @@ export function useChat({
     }
   }
 
-  const append: UseChatHelpers['append'] = async (message, options) => {
+  const append: UseChatHelpers['append'] = async (message, 
+    chatOptions?:ChatRequestOptions) => {
     if (!message.id) {
       message.id = nanoid()
     }
-    return triggerRequest(get(messages).concat(message as Message), options)
+    const{options, functions, function_call}=chatOptions??{};
+    const chatRequest: ChatRequest = {
+      messages: get(messages).concat(message as Message),
+      options,
+      ...(functions !== undefined && { functions }),
+      ...(function_call !== undefined && { function_call })
+    }
+    return triggerRequest(chatRequest)
   }
 
-  const reload: UseChatHelpers['reload'] = async options => {
+  const reload: UseChatHelpers['reload'] = 
+  async ({ options, functions, function_call }: ChatRequestOptions = {}) => {
     const messagesSnapshot = get(messages)
     if (messagesSnapshot.length === 0) return null
 
-    const lastMessage = messagesSnapshot[messagesSnapshot.length - 1]
-    if (lastMessage.role === 'assistant') {
-      return triggerRequest(messagesSnapshot.slice(0, -1), options)
+    // Remove last assistant message and retry last user message.
+    const lastMessage = messagesSnapshot.at(-1)
+    if (lastMessage?.role === 'assistant') {
+      const chatRequest: ChatRequest = {
+      messages: messagesSnapshot.slice(0, -1),
+      options,
+      ...(functions !== undefined && { functions }),
+      ...(function_call !== undefined && { function_call })
+      }
+
+      return triggerRequest(chatRequest)
     }
-    return triggerRequest(messagesSnapshot, options)
   }
 
   const stop = () => {
@@ -233,15 +332,20 @@ export function useChat({
 
   const input = writable(initialInput)
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (
+    e: any,
+    options: ChatRequestOptions = {}
+      ) => {
     e.preventDefault()
     const inputValue = get(input)
     if (!inputValue) return
-    append({
+    append(
+      {
       content: inputValue,
       role: 'user',
       createdAt: new Date()
-    })
+      },
+      options)
     input.set('')
   }
 
